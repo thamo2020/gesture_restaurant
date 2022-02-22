932d6d2b652af02649b5192ea205e6b6eeea3731
thamo2020 <thamodya01@gmail.com>
2022-02-22 23:52:17 +0530

    adding smallsreen sidebar menu and completing the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import {GiHumburgerMenu} from 'react-icons/gi';
-import {MdOutlineRestuarantMenu} from 'react-icons/md'
+import React, { useState } from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md'
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+
   <nav className="app__navbar">
       <div className='app__navbar-logo' >
       <img src={images.gericht} alt="app-logo" />
@@ -21,8 +26,25 @@ const Navbar = () => (
       <div />
       <a href="/" className='p__opensans'>Book Table</a>
       </div>
-      
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() =>setToggleMenu(true)}/>
+          
+          {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() =>setToggleMenu(false)}   />
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'> <a href="#home">Home</a> </li>
+              <li className='p__opensans'> <a href="#about">about</a> </li>
+              <li className='p__opensans'> <a href="#menu">menu</a> </li>
+              <li className='p__opensans'> <a href="#awards">awards</a> </li>
+              <li className='p__opensans'> <a href="#contact">contact</a> </li>
+            </ul>
+          </div>
+        )}
+        
+      </div>
   </nav>
-);
+)
+};
 
 export default Navbar;
