d7d11332dd2fc9c436f83e04de57470c89d90706
thamo2020 <thamodya01@gmail.com>
2022-02-22 11:11:25 +0530

    styling navbar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,19 @@ import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <nav class= "app__navbar">
-    <div className='app__navbar-logo' >
+  <nav className="app__navbar">
+      <div className='app__navbar-logo' >
       <img src={images.gericht} alt="app-logo" />
       </div>
+      <ul className='app__navbar--links'>
+        <li className='p__opensans'> <a href="#home">Home</a> </li>
+        <li className='p__opensans'> <a href="#about">about</a> </li>
+        <li className='p__opensans'> <a href="#menu">menu</a> </li>
+        <li className='p__opensans'> <a href="#awards">awards</a> </li>
+        <li className='p__opensans'> <a href="#contact">contact</a> </li>
+      </ul>
+
+      
   </nav>
 );
 
